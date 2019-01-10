5be2a7d55622e0532a92fa7d07f4312956b47ba4
xiaoxinga <xiaoxinga@yonyou.com>
2019-01-10 10:52:27 +0800

    代码

--- service/CrawlService.py
+++ service/CrawlService.py
@@ -12,3 +12,11 @@ pprint.pprint(count)#漂亮打印
 spam = ' Hello World '
 newspam = spam.strip()
 pprint.pprint(newspam)
+
+def printPicnic(itemsDict, leftWidth, rightWidth):
+ print('PICNIC ITEMS'.center(leftWidth + rightWidth, '-'))
+ for k, v in itemsDict.items():
+  print(k.ljust(leftWidth, '.') + str(v).rjust(rightWidth))
+picnicItems = {'sandwiches': 4, 'apples': 12, 'cups': 4, 'cookies': 8000}
+printPicnic(picnicItems, 12, 5)
+printPicnic(picnicItems, 20, 6)
